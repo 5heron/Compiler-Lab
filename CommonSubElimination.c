c38e8c03e4d5718827852dc638a2ef02409c243f
royalkuriyakosem <royalkuriyakosem@gmail.com>
2025-10-31 04:01:42 +0530

    Update CommonSubElimination.c

--- CommonSubElimination.c
+++ CommonSubElimination.c
@@ -1,93 +1,137 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-typedef struct {
+#define MAX 100
+
+struct Code {
     char op;
-    char arg1[10];
-    char arg2[10];
-    char result[10];
-    int isDead;
-} Quadruple;
+    char arg1[MAX];
+    char arg2[MAX];
+    char res[MAX];
+    int used;
+} code[MAX];
+
+int codeCount = 0;
+
+// Constant Folding
+void constant_folding() {
+    for (int i = 0; i < codeCount; i++) {
+        char *a1 = code[i].arg1;
+        char *a2 = code[i].arg2;
+
+        if (isdigit(a1[0]) && isdigit(a2[0])) {
+            int x = atoi(a1);
+            int y = atoi(a2);
+            int result = 0;
+
+            switch (code[i].op) {
+                case '+': result = x + y; break;
+                case '-': result = x - y; break;
+                case '*': result = x * y; break;
+                case '/': result = (y != 0) ? x / y : 0; break;
+                default: break;
+            }
+
+            sprintf(code[i].arg1, "%d", result);
+            strcpy(code[i].arg2, "-");
+            code[i].op = '=';
+        }
+    }
+}
+
+// Common Subexpression Elimination
+void common_subexpression_elimination() {
+    for (int i = 0; i < codeCount; i++) {
+        for (int j = i + 1; j < codeCount; j++) {
+            if (code[i].op == code[j].op &&
+                strcmp(code[i].arg1, code[j].arg1) == 0 &&
+                strcmp(code[i].arg2, code[j].arg2) == 0) {
+                
+                for (int k = j + 1; k < codeCount; k++) {
+                    if (strcmp(code[k].arg1, code[j].res) == 0)
+                        strcpy(code[k].arg1, code[i].res);
+                    if (strcmp(code[k].arg2, code[j].res) == 0)
+                        strcpy(code[k].arg2, code[i].res);
+                }
+                code[j].used = 0; // Mark duplicate
+            }
+        }
+    }
+}
+
+// Dead Code Elimination
+void dead_code_elimination() {
+    for (int i = 0; i < codeCount; i++) {
+        for (int j = 0; j < codeCount; j++) {
+            if (strcmp(code[j].arg1, code[i].res) == 0 ||
+                strcmp(code[j].arg2, code[i].res) == 0) {
+                code[i].used = 1;
+                break;
+            }
+        }
+    }
+
+    printf("\nOptimized Code:\n");
+    printf("OPR\tA1\tA2\tRES\n");
+    for (int i = 0; i < codeCount; i++) {
+        if (code[i].used || i == codeCount - 1) {
+            printf("%c\t%s\t%s\t%s\n",
+                   code[i].op, code[i].arg1, code[i].arg2, code[i].res);
+        }
+    }
+}
 
 int main() {
-    FILE *fin, *fout;
-    fin = fopen("inputExpressions.txt", "r");
-    fout = fopen("opt2code.txt", "w");
-    if (!fin) {
-        printf("Error opening file.\n");
-        return 1;
+    printf("Enter number of 3-address code instructions: ");
+    scanf("%d", &codeCount);
+
+    printf("\nEnter 3-address code (OP A1 A2 RES):\n");
+    for (int i = 0; i < codeCount; i++) {
+        scanf(" %c %s %s %s", &code[i].op, code[i].arg1, code[i].arg2, code[i].res);
+        code[i].used = 0;
     }
 
-    Quadruple q[100];
-    int n = 0;
-
-    char line[100];
-    printf("%-9s %-9s %-9s %-9s\n", "Operator", "Arg1", "Arg2", "Result");
-    printf("------------------------------------------------\n");
-    while (fgets(line, sizeof(line), fin)) {
-        q[n].isDead = 0;
-        if (sscanf(line, " %s = %s %c %s", q[n].result, q[n].arg1, &q[n].op, q[n].arg2) == 4) {
-            // Format: a = b + c
-        }
-        else if (sscanf(line, " %s = %s", q[n].result, q[n].arg1) == 2) {
-            q[n].op = '=';
-            strcpy(q[n].arg2, "-");
-        }
-        printf("%-9c %-9s %-9s %-9s\n", q[n].op, q[n].arg1, q[n].arg2, q[n].result); 
-        n++;
+    printf("\n--- Original Code ---\n");
+    printf("OPR\tA1\tA2\tRES\n");
+    for (int i = 0; i < codeCount; i++) {
+        printf("%c\t%s\t%s\t%s\n",
+               code[i].op, code[i].arg1, code[i].arg2, code[i].res);
     }
 
-    fclose(fin);
-
-    // --- Common Subexpression Elimination (safe) ---
-    for (int i = 0; i < n; i++) {
-        if (q[i].op == '=') continue;
-        for (int j = i + 1; j < n; j++) {
-            if (q[i].op == q[j].op &&
-                strcmp(q[i].arg1, q[j].arg1) == 0 &&
-                strcmp(q[i].arg2, q[j].arg2) == 0) {
-
-                // Replace later uses of q[j].result with q[i].result
-                for (int k = j + 1; k < n; k++) {
-                    if (strcmp(q[j].result, q[k].arg1) == 0)
-                        strcpy(q[k].arg1, q[i].result);
-                    if (strcmp(q[j].result, q[k].arg2) == 0)
-                        strcpy(q[k].arg2, q[i].result);
-                    //if either result vars redefined
-                    if (strcmp(q[j].result, q[k].result) == 0 || strcmp(q[i].result, q[k].result) == 0) break;
-                }
-                q[j].isDead = 1;
-            }
-            //if either arg of common subexpression is redefined
-            if (strcmp(q[j].result, q[i].arg1) == 0 || strcmp(q[j].result, q[i].arg2) == 0) break;
-            //if result is redefined
-            if (strcmp(q[j].result, q[i].result) == 0) break; 
-        }
-    }
-
-    // --- Dead Code Elimination ---
-    // for (int i = 0; i < n; i++) {
-    //     int used = 0;
-    //     for (int j = i + 1; j < n; j++) {
-    //         if (strcmp(q[i].result, q[j].arg1) == 0 ||
-    //             strcmp(q[i].result, q[j].arg2) == 0) {
-    //             used = 1;
-    //             break;
-    //         }
-    //     }
-    //     if (!used) q[i].isDead = 1;
-    // }
-
-    printf("\nOptimized Code :\n");
-    printf("%-9s %-9s %-9s %-9s\n", "Operator", "Arg1", "Arg2", "Result");
-    printf("------------------------------------------------\n");
-    for (int i = 0; i < n; i++) {
-        if (!q[i].isDead){
-            printf("%-9c %-9s %-9s %-9s\n", q[i].op, q[i].arg1, q[i].arg2, q[i].result);
-            fprintf(fout, "%-9c %-9s %-9s %-9s\n", q[i].op, q[i].arg1, q[i].arg2, q[i].result);
-        }
-    }
+    constant_folding();
+    common_subexpression_elimination();
+    dead_code_elimination();
 
     return 0;
 }
+
+/* OUTPUT
+developer@ccfl2-pc7:~$ ./a.out
+Enter number of 3-address code instructions: 6
+Enter 3-address code (OP A1 A2 RES):
++ 2 3 t1
++ 4 5 t2
++ t1 t2 t3
++ 2 3 t4
++ t3 t4 t5
+= t5 - a
+
+--- Original Code ---
+OPR     A1      A2      RES
++       2       3       t1
++       4       5       t2
++       t1      t2      t3
++       2       3       t4
++       t3      t4      t5
+=       t5      -       a
+
+Optimized Code:
+OPR     A1      A2      RES
+=       5       -       t1
+=       9       -       t2
++       t1      t2      t3
++       t3      t1      t5
+=       t5      -       a
+*/
